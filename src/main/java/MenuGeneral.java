804ad3acbcdc079de2c10220d0baf4948672532c
joanF <jsfarfans@udistrital.edu.co>
2025-12-10 14:46:04 -0500

    Creacion de menus

---
+++ src/main/java/MenuGeneral.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class MenuGeneral {
+    private JPanel PanelPrin;
+    private JTextField bienvenidoTextField;
+    private JButton OpMatbtn;
+    private JButton ManArbtn;
+    private JButton JyEbtn;
+    private JButton ConvNumbtn;
+    private JButton Salirbtn;
+    private JButton Procadbtn;
+}
