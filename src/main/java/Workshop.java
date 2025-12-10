49facf59e54c6b5384f3f5b580609cee453952fc
joanF <jsfarfans@udistrital.edu.co>
2025-12-10 14:03:57 -0500

    decimotercero test realizado

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -181,7 +181,18 @@ public class Workshop {
     public int[] ordenarArreglo(int[] arreglo) {
         // TODO: Implementar el método para ordenar un arreglo en orden ascendente.
         // Ejemplo: Si arreglo = [5, 4, 3, 2, 1], el resultado debería ser [1, 2, 3, 4, 5].
-        return new int[0];
+        int orden = 0;
+        for (int i = 0; i < arreglo.length-1; i++) {
+            for (int j = 0; j < arreglo.length-i-1; j++) {
+                if (arreglo[j] > arreglo[j+1]) {
+                    orden = arreglo[j];
+                    arreglo[j] = arreglo[j+1];
+                    arreglo[j+1] = orden;
+                }
+            }
+        }
+
+        return arreglo;
     }
 
     // Método que elimina los duplicados de un arreglo
