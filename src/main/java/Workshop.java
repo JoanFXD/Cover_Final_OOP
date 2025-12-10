7653f66af37a9372c28ee10eab8d6264bafd66ce
joanF <jsfarfans@udistrital.edu.co>
2025-12-10 13:45:15 -0500

    duodecimo test realizado

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -170,9 +170,13 @@ public class Workshop {
     public int[] invertirArreglo(int[] arreglo) {
         // TODO: Implementar el método para invertir un arreglo.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser [5, 4, 3, 2, 1].
-        return new int[0];
-    }
+        int[] invertido = new  int[arreglo.length];
+        for (int i = 0; i < arreglo.length; i++) {
+            invertido[i] = arreglo[arreglo.length-i-1];
+        }
 
+        return invertido;
+    }
     // Método que ordena un arreglo en orden ascendente
     public int[] ordenarArreglo(int[] arreglo) {
         // TODO: Implementar el método para ordenar un arreglo en orden ascendente.
